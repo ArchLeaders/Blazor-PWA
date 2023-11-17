1dbebea4c20b342fa4a6b3e581f60c764358fb32
ArchLeaders <archleadership28@gmail.com>
2023-11-17 10:56:03 -0800

    Add PWA updater

--- BlazorWebAssembly/Program.cs
+++ BlazorWebAssembly/Program.cs
@@ -14,5 +14,6 @@ builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSpeechRecognitionServices();
 builder.Services.AddSpeechSynthesis();
+builder.Services.AddPWAUpdater();
 
 await builder.Build().RunAsync();
