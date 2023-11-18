9f5b6d8864e65f834c3561e62635af74e45dfe3a
ArchLeaders <archleadership28@gmail.com>
2023-11-18 02:13:40 -0800

    Implement custom STT

---
+++ BlazorWebAssembly/Controls/SpeechToTextButton.razor.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+
+
+namespace BlazorWebAssembly.Controls;
+
+// Derived from https://github.com/radzenhq/radzen-blazor/
+// Copyright (c) 2018-2023 Radzen Ltd
+
+/// <summary>
+/// Simple button for recording and transcripting speech to text
+/// <br/><br/>
+/// Derived from <a href="https://github.com/radzenhq/radzen-blazor/">Radzen Blazor</a><br/>
+/// Copyright (c) 2018-2023 Radzen Ltd
+/// </summary>
+public partial class SpeechToTextButton : MudComponentBase
+{
+    /// <summary>
+    /// The color of the component. It supports the theme colors.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.Appearance)]
+    public Color Color { get; set; } = Color.Default;
+
+    /// <summary>
+    /// The Size of the component.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.Appearance)]
+    public Size Size { get; set; } = Size.Medium;
+
+    /// <summary>
+    /// The variant to use.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.Appearance)]
+    public Variant Variant { get; set; } = Variant.Filled;
+
+    /// <summary>
+    /// Child content of component.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.Behavior)]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// Icon placed before the text if set.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.Behavior)]
+    public string? Icon { get; set; } = Icons.Material.Filled.Mic;
+
+    /// <summary>
+    /// Gets or sets the icon displayed while recording.
+    /// </summary>
+    /// <value>The icon.</value>
+    [Parameter]
+    public string StopIcon { get; set; } = Icons.Material.Filled.Stop;
+
+    private string? CurrentIcon => _recording ? StopIcon : Icon;
+
+    /// <summary>
+    /// Gets or sets the message displayed when user hovers the button and it is not recording.
+    /// </summary>
+    /// <value>The message.</value>
+    [Parameter]
+    public string Title { get; set; } = "Press to start speech recognition";
+
+    /// <summary>
+    /// Gets or sets the message displayed when user hovers the button and it is recording.
+    /// </summary>
+    /// <value>The message.</value>
+    [Parameter]
+    public string StopTitle { get; set; } = "Press to stop speech recognition";
+
+    private string CurrentTitle => _recording ? StopTitle : Title;
+
+
+    /// <summary>
+    /// Callback which provides results from the speech recognition API.
+    /// </summary>
+    [Parameter]
+    public EventCallback<string> Change { get; set; }
+
+    /// <summary>
+    /// Gets or sets the icon displayed while recording.
+    /// </summary>
+    /// <value>The icon.</value>
+    [Parameter]
+    public required string Language { get; set; }
+
+    private bool _recording;
+
+    private async Task StartSpeechToText()
+    {
+        _recording = !_recording;
+
+        await JSRuntime.InvokeVoidAsync("BlazorWebAssembly.toggleDictation", Reference, Language);
+    }
+
+    /// <summary>
+    /// Provides interface for javascript to stop speech to text recording on this component if another component starts recording.
+    /// </summary>
+    [JSInvokable]
+    public void StopRecording()
+    {
+        _recording = false;
+
+        StateHasChanged();
+    }
+
+    /// <summary>
+    /// Provides interface for javascript to pass speech results back to this component.
+    /// </summary>
+    /// <param name="result"></param>
+    [JSInvokable]
+    public void OnResult(string result)
+    {
+        Change.InvokeAsync(result);
+    }
+
+    private DotNetObjectReference<SpeechToTextButton>? _reference;
+
+    /// <summary>
+    /// Gets the reference for the current component.
+    /// </summary>
+    /// <value>The reference.</value>
+    protected DotNetObjectReference<SpeechToTextButton> Reference
+        => _reference ??= DotNetObjectReference.Create(this);
+}
---
+++ BlazorWebAssembly/wwwroot/script/SpeechRecognition.js
@@ -0,0 +1,72 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2018-2023 Radzen Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+var recognition;
+
+window.BlazorWebAssembly = {
+    toggleDictation: function (componentRef, language) {
+        function start() {
+            const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+            if (!SpeechRecognition) {
+                return;
+            }
+
+            recognition = new SpeechRecognition();
+            recognition.componentRef = componentRef;
+            recognition.continuous = true;
+
+            if (language) {
+                recognition.lang = language;
+            }
+
+            recognition.onresult = function (event) {
+                if (event.results.length < 1) {
+                    return;
+                }
+
+                let current = event.results[event.results.length - 1][0]
+                let result = current.transcript;
+
+                componentRef.invokeMethodAsync("OnResult", result);
+            };
+            recognition.onend = function (event) {
+                componentRef.invokeMethodAsync("StopRecording");
+                recognition = null;
+            };
+            recognition.start();
+        }
+
+        if (recognition) {
+            if (recognition.componentRef._id != componentRef._id) {
+                recognition.addEventListener('end', start);
+            }
+            recognition.stop();
+        } else {
+            start();
+        }
+    }
+}
